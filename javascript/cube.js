ac269f5f72428d6b2cb9162d1ac8d03bbb779f4c
Swiatlon <wiercikdoc@gmail.com>
2020-04-23 19:58:21 +0200

    Added flag for doublet

--- javascript/cube.js
+++ javascript/cube.js
@@ -5,19 +5,25 @@ class Cube {
   }
 
   getNumberRandom(max, min) {
+    
     let firstThrown = Math.floor(Math.random() * (7 - 1) + 1); //7 -max  1 -min
     let secondThrown = Math.floor(Math.random() * (7 - 1) + 1);
-    // if (number == secondNumber) {
-    //   // console.log('[dublet]');
-    // } else if (number !== secondNumber) {
-    //   // console.log('[Nie ma Doubletu]');
-    // }
+    if (firstThrown == secondThrown) {
+      // console.log('[dublet]');
+     doublet = true;
+
+    } else{
+      doublet = false;
+    }
+ 
     if (this.lastThrows[0] !== firstThrown) {
-      this.animationOfCube(firstThrown, this.cubes[0]);
-      console.log("[lastThrown]", this.lastThrows);
+        // flaga do animacji kostki zeby nie powtarzalo funkcji na koncu animacji
+      this.animationOfCube(firstThrown, this.cubes[0]); 
     } else {
+      flag = false;   // Animacja resetujaca nie wywoluje funkcji 
       this.animationOfCube(10, this.cubes[0]);
       setTimeout(() => {
+        flag = true;    // Animacja  kiedy  juz  jest numer ktory powinien byc.Kostka sie juz nie kreci
         this.animationOfCube(firstThrown, this.cubes[0]);
       }, 1005);
     }
@@ -37,7 +43,10 @@ class Cube {
     if (playerQueue > game.players.length - 1) {
       playerQueue = 0;
     }
+    
+    
     game.players[playerQueue].amountOfMoves = firstThrown + secondThrown;
+    
   }
 
   animationOfCube(number, cube) {
@@ -62,7 +71,11 @@ class Cube {
         break;
       default:
         cube.style.webkitTransform = "rotateX(-250deg) rotateY(1500deg)";
+       
     }
+   
+    
+    
   }
 
   getCubes() {
