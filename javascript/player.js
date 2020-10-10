fdf8a96748f213671567dc31a5d74af7a928fe56
Swiatlon <wiercikdoc@gmail.com>
2020-10-10 18:53:58 +0200

    formatted the code

--- javascript/player.js
+++ javascript/player.js
@@ -1,61 +1,61 @@
 class Player {
-    constructor(nameOfPlayer, position, color) {
-        this.nameOfPlayer = nameOfPlayer;
-        this.field = 0;
-        this.amountOfMoves = 0;
-        this.id = position;
-        this.image = "images/player" + position + ".png";
-        this.positionInQueue = position;
-        this.cities = [];
-        this.money = 3000;
-        this.color = color;
-        this.jail = false;
-        this.img = document.createElement("img");
-        this.img.width =60;
-        this.img.height = 60;
-        this.img.src = this.image;
-        this.img.style.marginLeft = "auto";
-        this.img.style.marginRight = "auto";
-        this.img.style.zIndex = '1';
-        this.img.style.position = "Relative";
-        this.img.style.transform =  " rotate3d(0, 1, 0.5, 0.4rad)";
-        this.img.imgIdentyficator = this.nameOfPlayer;
-        this.counterOfStayingInJail = 0;
-        this.tryingDoublet = false;
-        this.teleporting = false;
-    }
+  constructor(nameOfPlayer, position, color) {
+    this.nameOfPlayer = nameOfPlayer;
+    this.field = 0;
+    this.amountOfMoves = 0;
+    this.id = position;
+    this.image = "images/player" + position + ".png";
+    this.positionInQueue = position;
+    this.cities = [];
+    this.money = 3000;
+    this.color = color;
+    this.jail = false;
+    this.img = document.createElement("img");
+    this.img.width = 60;
+    this.img.height = 60;
+    this.img.src = this.image;
+    this.img.style.marginLeft = "auto";
+    this.img.style.marginRight = "auto";
+    this.img.style.zIndex = '1';
+    this.img.style.position = "Relative";
+    this.img.style.transform = " rotate3d(0, 1, 0.5, 0.4rad)";
+    this.img.imgIdentyficator = this.nameOfPlayer;
+    this.counterOfStayingInJail = 0;
+    this.tryingDoublet = false;
+    this.teleporting = false;
+  }
 
-    move() {
-        let fieldBeforeAnimationOfMove = this.field;    
-        const lastField = 31;
-        const firstField = 0;
-        let mathAlgorithm = lastField - fieldBeforeAnimationOfMove;        // There is my algorithm to calculate how much fields we need to the last field 
-        btn.disabled = true;            
-        let playerAnimationOfMove = setInterval(() => {
-            if (this.field !== fieldBeforeAnimationOfMove + this.amountOfMoves) {       //  Moving player untill end of movement
-                this.field++;
-                if (this.field > lastField) {  // After last field (31) we go to the start (0)
-                    this.field = firstField;
-                    this.money += 300;
-                    map.visualAmountOfMoney(this);
-                }
-                if (this.field == firstField) {
-                    fieldBeforeAnimationOfMove = firstField;
-                    this.amountOfMoves = this.amountOfMoves - mathAlgorithm - 1; 
-                }
-                map.allLands[this.field].children[0].appendChild(this.img);
-            } else {    
-                clearInterval(playerAnimationOfMove);
-                this.endOfMoveAnimation();
-            }
-        }, 300);
-    }
-
-    endOfMoveAnimation() {
-        if(this.teleporting == false){
-            game.gameMechanism(this);
+  move() {
+    let fieldBeforeAnimationOfMove = this.field;
+    const lastField = 31;
+    const firstField = 0;
+    let mathAlgorithm = lastField - fieldBeforeAnimationOfMove; // There is my algorithm to calculate how much fields we need to the last field 
+    btn.disabled = true;
+    let playerAnimationOfMove = setInterval(() => {
+      if (this.field !== fieldBeforeAnimationOfMove + this.amountOfMoves) { //  Moving player untill end of movement
+        this.field++;
+        if (this.field > lastField) { // After last field (31) we go to the start (0)
+          this.field = firstField;
+          this.money += 300;
+          map.visualAmountOfMoney(this);
         }
+        if (this.field == firstField) {
+          fieldBeforeAnimationOfMove = firstField;
+          this.amountOfMoves = this.amountOfMoves - mathAlgorithm - 1;
+        }
+        map.allLands[this.field].children[0].appendChild(this.img);
+      } else {
+        clearInterval(playerAnimationOfMove);
+        this.endOfMoveAnimation();
+      }
+    }, 300);
+  }
+
+  endOfMoveAnimation() {
+    if (this.teleporting == false) {
+      game.gameMechanism(this);
     }
-    
+  }
+
 
 }
